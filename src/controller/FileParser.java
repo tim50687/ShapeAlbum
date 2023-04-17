e07c725180cdcdd72ca73dc78100194465f793e5
Jen Ting Huang <jentinghuang@jens-mbp.mynetworksettings.com>
2023-04-17 02:53:05 -0400

    Implemented file parser

--- src/controller/FileParser.java
+++ src/controller/FileParser.java
@@ -1,7 +1,13 @@
 package controller;
 
 import album.Canvas;
+import command.ChangeColorShape;
+import command.ChangeSizeShape;
 import command.Command;
+import command.MoveShape;
+import command.PutShape;
+import command.RemoveShape;
+import command.SnapShotCanvas;
 import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -11,6 +17,7 @@ import java.util.Scanner;
 import shape.ICoordinate;
 import shape.IShape;
 import shape.ShapeFactory;
+import shape.twoDCoordinate;
 
 public class FileParser {
 
@@ -57,23 +64,50 @@ public class FileParser {
         String command = words[0];
         switch (command.toUpperCase()) {
           case "SHAPE":
-//            coordinate = new twoDCoordinate(Double.parseDouble(words[3]),
-//                Double.parseDouble(words[4]));
-//            color = new Color(Integer.parseInt(words[7]), Integer.parseInt(words[8]),
-//                Integer.parseInt(words[9]));
-//            name = words[1];
-//            type = words[2];
-//            width = Integer.parseInt(words[5]);
-//            height = Integer.parseInt(words[6]);
-//            shape = factory.getShape(type, name, coordinate, color, width, height);
-//            commandClass = new PutShape(this.canvas, type, name, shape);
-//            this.commands.add(commandClass);
+            coordinate = new twoDCoordinate(Integer.parseInt(words[3]),
+                Integer.parseInt(words[4]));
+            color = new Color(Integer.parseInt(words[7]), Integer.parseInt(words[8]),
+                Integer.parseInt(words[9]));
+            name = words[1];
+            type = words[2];
+            width = Integer.parseInt(words[5]);
+            height = Integer.parseInt(words[6]);
+            shape = factory.getShape(type, name, coordinate, color, width, height);
+            commandClass = new PutShape(this.canvas, name, shape);
+            this.commands.add(commandClass);
             break;
           case "MOVE":
+            coordinate = new twoDCoordinate(Integer.parseInt(words[2]),
+                Integer.parseInt(words[3]));
+            name = words[1];
+            commandClass = new MoveShape(this.canvas, name, coordinate);
+            this.commands.add(commandClass);
+            break;
           case "COLOR":
+            color = new Color(Integer.parseInt(words[2]), Integer.parseInt(words[3]),
+                Integer.parseInt(words[4]));
+            name = words[1];
+            commandClass = new ChangeColorShape(this.canvas, name, color);
+            this.commands.add(commandClass);
+            break;
           case "RESIZE":
+            name = words[1];
+            width = Integer.parseInt(words[2]);
+            height = Integer.parseInt(words[3]);
+            commandClass = new ChangeSizeShape(this.canvas, name, width, height);
+            this.commands.add(commandClass);
+            break;
           case "REMOVE":
+            name = words[1];
+            commandClass = new RemoveShape(this.canvas, name);
+            this.commands.add(commandClass);
+            break;
           case "SNAPSHOT":
+            String[] temp = line.split("\\s+", 2);
+            String description = temp.length > 1 ? temp[1] : "";
+            commandClass = new SnapShotCanvas(this.canvas, description);
+            this.commands.add(commandClass);
+            break;
         }
         System.out.println(words[0]);
       }
@@ -84,25 +118,5 @@ public class FileParser {
     return this.commands;
   }
 
-  public static void main(String[] args) {
-    File inputfile = new File("demo_input.txt");
-    String[] words;
-    String line = null;
-    try {
-      Scanner fileIn = new Scanner(inputfile);
-      while (fileIn.hasNext()) {
-        line = fileIn.nextLine().trim();
-        if (line.isEmpty()) {
-          continue;
-        }
-        if (line.startsWith("#")) {
-          continue;
-        }
-        words = line.split("\\s+");
-        System.out.println(words[0]);
-      }
-    } catch (FileNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-  }
 }
+
