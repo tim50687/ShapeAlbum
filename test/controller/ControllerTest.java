75cb664b33fbf159ec4cfc18d8c76a646c4a91f3
Jen Ting Huang <jentinghuang@jens-mbp.mynetworksettings.com>
2023-04-17 20:39:51 -0400

    Created test case for controller and model

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -1,5 +1,7 @@
 package controller;
 
+import static org.junit.Assert.assertEquals;
+
 import model.Canvas;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,14 +20,69 @@ public class ControllerTest {
   }
 
   @Test
-  public void testRun() {
+  public void testRunOnDemo() {
     inputFile = "demo_input.txt";
     controller = new Controller(webAlbum, inputFile, canvas);
     controller.run();
     // Test the number of snapshots
-//    assertEquals(4, canvas.getAllSnapshot().size());
-          
-    canvas.printSnapshots();
+    assertEquals(4, canvas.getAllSnapshot().size());
+    // Test the shape inside the canvas
+    assertEquals(1, canvas.getAllShape().size());
+    // Test the oval that still on the canvas
+    // Coordinate
+    assertEquals(500, this.canvas.getShape("myoval").getCoordinate().getX());
+    assertEquals(400, this.canvas.getShape("myoval").getCoordinate().getY());
+    // Size
+    assertEquals(60, this.canvas.getShape("myoval").getWidth());
+    assertEquals(30, this.canvas.getShape("myoval").getHeight());
+
+  }
+
+  @Test
+  public void testRunOnBuildings() {
+    inputFile = "buildings.txt";
+    controller = new Controller(webAlbum, inputFile, canvas);
+    controller.run();
+    // Test the number of snapshots
+    assertEquals(3, canvas.getAllSnapshot().size());
+    // Test the shape inside the canvas
+    assertEquals(22, canvas.getAllShape().size());
+    // Test shape on the canvas
+    // Coordinate
+    assertEquals(0, this.canvas.getShape("background").getCoordinate().getX());
+    assertEquals(0, this.canvas.getShape("background").getCoordinate().getY());
+
+    assertEquals(620, this.canvas.getShape("B3").getCoordinate().getX());
+    assertEquals(445, this.canvas.getShape("B3").getCoordinate().getY());
+
+    assertEquals(280, this.canvas.getShape("window002").getCoordinate().getX());
+    assertEquals(500, this.canvas.getShape("window002").getCoordinate().getY());
+
+    assertEquals(460, this.canvas.getShape("window003").getCoordinate().getX());
+    assertEquals(500, this.canvas.getShape("window003").getCoordinate().getY());
+
+    assertEquals(640, this.canvas.getShape("window004").getCoordinate().getX());
+    assertEquals(500, this.canvas.getShape("window004").getCoordinate().getY());
+
+    // Size
+    assertEquals(800, this.canvas.getShape("background").getWidth());
+    assertEquals(800, this.canvas.getShape("background").getHeight());
+
+    assertEquals(100, this.canvas.getShape("B3").getWidth());
+    assertEquals(305, this.canvas.getShape("B3").getHeight());
+
+    assertEquals(20, this.canvas.getShape("window002").getWidth());
+    assertEquals(20, this.canvas.getShape("window002").getHeight());
+
+    assertEquals(20, this.canvas.getShape("window003").getWidth());
+    assertEquals(20, this.canvas.getShape("window003").getHeight());
+
+    assertEquals(20, this.canvas.getShape("window004").getWidth());
+    assertEquals(20, this.canvas.getShape("window004").getHeight());
+
+    assertEquals(20, this.canvas.getShape("window044").getWidth());
+    assertEquals(20, this.canvas.getShape("window044").getHeight());
+
   }
 
 
